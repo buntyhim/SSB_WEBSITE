d3356d7de7f8841d7fa6b6743b66871cb7c0edc2
buntyhim <bunty.him@live.com>
2021-04-03 14:44:35 +0530

    removed timer from lecturette

--- src/main.js
+++ src/main.js
@@ -147,9 +147,6 @@ function showNewTopics(){
     document.getElementById("topic3").value = random_topic();
     document.getElementById("topic4").value = random_topic();
 
-     var threeMinutes = 60 * 3,
-        display = document.querySelector('#timer');
-    startTimer(threeMinutes, display);
 }
 
 function loadModel(){
@@ -157,21 +154,3 @@ function loadModel(){
     
 }
 
-
-function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = minutes + ":" + seconds;
-
-        if (--timer < 0) {
-            timer = duration;
-            showNewTopics();
-        }
-    }, 1000);
-}
